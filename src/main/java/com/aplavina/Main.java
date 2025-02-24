0473c97175dee1290c79a183155a6027c0ee0b4e
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2025-02-24 09:25:24 +0300

    using GeometryLibrary version 1.1.0

--- src/main/java/com/aplavina/Main.java
+++ src/main/java/com/aplavina/Main.java
@@ -8,5 +8,11 @@ public class Main {
         Figure figure = new Triangle(2, 3, 4);
         System.out.println("area: " + figure.area());
         System.out.println("perimeter: " + figure.perimeter());
+
+        try {
+            Figure invalid = new Triangle(2, 3, 5);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Caught IllegalArgumentException");
+        }
     }
 }
